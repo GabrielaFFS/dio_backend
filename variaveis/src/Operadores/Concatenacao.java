d57ef0670d89ab832f306e8a9caa57bb4dd6424d
GabrielaFFS <gabylla8582@gmail.com>
2024-05-19 14:40:33 -0300

    Desafios

---
+++ variaveis/src/Operadores/Concatenacao.java
@@ -0,0 +1,8 @@
+package Operadores;
+
+public class Operadores {
+     public static void main(String[] args) {
+        String nomeCompleto = "GABRIELA" + " Freire";
+        System.out.println(nomeCompleto);
+     }
+}
