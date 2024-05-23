e2a575e4a7c4930a2a7151642e998398fc43d3c3
GabrielaFFS <gabylla8582@gmail.com>
2024-05-23 10:26:32 -0300

    Exemplos de controle de fluxo

--- controleFluxo/src/CaixaEletronico.java
+++ controleFluxo/src/CaixaEletronico.java
@@ -3,12 +3,21 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         
         double saldo= 50.0;
-        double valorSolicitado= 30.0;
+        double valorSolicitado= 100.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo){
         saldo = saldo - valorSolicitado;
+        
+        System.out.println("O seu saldo atual é " + saldo);
+        }
 
-        System.out.println("O seu saldo atual é: " + saldo);
+
+        
+        else {
+            System.out.println("Saldo insuficiente");
+            
+        System.out.println("O seu saldo é " + saldo);
+        }
 
         
     }
---
+++ controleFluxo/src/ResultadoEscolar.java
@@ -0,0 +1,24 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+    
+    int nota=6 ;
+
+    if ((nota >= 7)) {
+        System.out.println("Aluno aprovado");
+        
+    } 
+    else if ( nota >= 5 && nota <7) { 
+
+        System.out.println("Aluno em recuperação");
+    }
+
+    else {
+        
+    System.out.println("Aluno reprovado");
+    }
+    
+
+    
+    }
+}
